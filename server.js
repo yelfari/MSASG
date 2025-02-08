6dc2cc3166d2046adeeb4f6446bbec304d6eff95
yelfari <y.elfari@yahoo.de>
2025-02-08 11:06:43 +0100

    new commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Handle requests to the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("hii");
+});

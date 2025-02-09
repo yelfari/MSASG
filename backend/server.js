4d7deca43e25b36b8aeebd0b8a21c3b7a9b3b00b
yelfari <y.elfari@yahoo.de>
2025-02-09 06:01:23 +0000

    new commit

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Handle requests to the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log("hii");

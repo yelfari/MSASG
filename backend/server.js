51e1145d1af889530f590a761c1feb200e3bc630
yelfari <y.elfari@yahoo.de>
2025-02-11 09:09:42 +0000

    get ips

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,13 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
+
+app.use((req, res, next) => {
+    // Log the client's IP address
+    console.log(`Client IP: ${req.ip}`);
+    next(); // Continue processing the request
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log("hii");
